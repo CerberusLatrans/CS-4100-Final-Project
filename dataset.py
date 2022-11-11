dffe3b8e50577094a3b84c0fda7d2417969f61eb
Garamon <ollietoh@gmail.com>
2022-11-11 16:51:08 -0500

    add coordinate generation

--- dataset.py
+++ dataset.py
@@ -7,6 +7,9 @@
 import requests
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+import urllib.parse
+import random
+import math
 
 # Enter your api key here
 api_key = "AIzaSyATvdjLWeqJ0svqAXNYmdAmVU7tkaz8434" # Prob want this as a google secret through GCP
@@ -32,7 +35,7 @@ map_id = "5fd53f1f4a5c6512"
 # get method of requests module
 # return response object
 full_url = url + "center=" + center + "&zoom=" + str(zoom) + "&size=" + size + "&maptype= " + maptype + "&key=" + api_key + "&map_id=" + map_id
-print(full_url)
+#print(full_url)
 r = requests.get(full_url)
   
 with open('map.jpg', 'wb') as file:
@@ -61,3 +64,40 @@ plt.show()
 
 # TODO: After we generate image, put into GCP bucket by making GCP API call
 
+# creates n uniformly random samples within radius r from the location
+def get_coords(location, n, radius):
+    url = 'https://nominatim.openstreetmap.org/search/' + urllib.parse.quote(location) +'?format=json'
+    response = requests.get(url).json()
+    anchor_lat = float(response[0]["lat"]) #y
+    anchor_lon = float(response[0]["lon"]) #x
+
+    #convert miles to lat degrees
+    r = radius / 69
+
+    coordinates = []
+
+    for _ in range(n):
+        #latitude can be +- radius from the anchor lat
+        lat = anchor_lat + random.uniform(-r, r)
+
+        # from circle eqn
+        delta_lon = math.sqrt(r**2 - (lat - anchor_lat)**2)
+        lon = anchor_lon + random.uniform(-delta_lon, delta_lon)
+
+        zoom = random.randint(17, 20)
+        coordinates.append((lat, lon, zoom))
+
+    #print(coordinates)
+    return coordinates
+
+
+def upload_to_cloud(c):
+    pass
+
+locations = ["New York", "Boston"]
+coords = []
+for l in locations:
+    coords.extend(get_coords(l, n=10, radius=5))
+for c in coords:
+    upload_to_cloud(c)
+    
