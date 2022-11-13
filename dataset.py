85f3ac09409f6df23145752e3406523d33e34030
Jessica Yu <jessicayu62@gmail.com>
2022-11-13 14:03:42 -0500

    refactor image querying process and layout foundation for rest of process

--- dataset.py
+++ dataset.py
@@ -1,82 +1,65 @@
 
-# Python program to get a google map 
-# image of specified location using 
-# Google Static Maps API
-  
-# importing required modules
 import requests
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
+# import matplotlib.pyplot as plt
+# import matplotlib.image as mpimg
 import random
 import math
 import urllib
 
-# Enter your api key here
-api_key = "AIzaSyATvdjLWeqJ0svqAXNYmdAmVU7tkaz8434" # Prob want this as a google secret through GCP
-  
-# url variable store url
-url = "https://maps.googleapis.com/maps/api/staticmap?"
-  
-# center defines the center of the map,
-# equidistant from all edges of the map. 
-center = "42.3398106,-71.0913604" # TODO: Automate generating coordinates
-  
-# zoom defines the zoom
-# level of the map
-zoom = 17
+API_KEY = "AIzaSyATvdjLWeqJ0svqAXNYmdAmVU7tkaz8434" # Prob want this as a google secret through GCP
+URL = "https://maps.googleapis.com/maps/api/staticmap?"
+SIZE = "640x640"
+MAP_ID = "5fd53f1f4a5c6512" # For styling the map -- clean map
+MAP_TYPE = "satellite" # for dirty map
+# center = "42.3398106,-71.0913604" <- northeastern university
+# labelsTextOff="feature:all|element:labels.text|visibility: off"
+# labelsIconOff="feature:all|element:labels.icon|visibility: off"
 
-maptype = "terrain"
+def get_map(center, zoom, file_name, type):
+    full_url = URL + "center=" + center + "&zoom=" + str(zoom) + "&size=" + SIZE + "&key=" + API_KEY
+    if (type == 'clean'):
+        full_url += "&map_id=" + MAP_ID 
+    if (type == 'dirty'):
+        full_url += "&maptype=" + MAP_TYPE
+    
+    r = requests.get(full_url)
+    with open('images/pre-canny/' + type + '/' + file_name, 'wb') as file:
+        file.write(r.content)
+    r.close()
 
-size = "640x640"
+    return file_name
+    # using matpltolib to display the image
+    # plt.figure(figsize=(5, 5))
+    # img=mpimg.imread('map.jpg') #render the terrain
+    # dirtyImg=mpimg.imread('dirtyMap.jpg') #render the styled map
+    # imgplot = plt.imshow(img)
+    # dirtyImgPlot = plt.imshow(dirtyImg)
+    # plt.axis('off')
+    # plt.show()
 
-# For styling the map
-map_id = "5fd53f1f4a5c6512"
-  
-# get method of requests module
-# return response object
-full_url = url + "center=" + center + "&zoom=" + str(zoom) + "&size=" + size + "&maptype= " + maptype + "&key=" + api_key + "&map_id=" + map_id
-print(full_url)
-r = requests.get(full_url)
-  
-with open('map.jpg', 'wb') as file:
-    file.write(r.content)
+def apply_canny(file_name): #TODO
+    """Applies canny filter to image"""
+    pass
 
-r.close()
+def upload_blob(source_file_name): #TODO
+    pass
+    """Uploads a file to the bucket."""
+    # The ID of your GCS bucket
+    # bucket_name = "your-bucket-name"
+    # The path to your file to upload
+    # source_file_name = "local/path/to/file"
+    # The ID of your GCS object
+    # destination_blob_name = "storage-object-name"
 
-# now we repeat this process but with dirty map
-maptype2 = "satellite"
-labelsTextOff="feature:all|element:labels.text|visibility: off"
-labelsIconOff="feature:all|element:labels.icon|visibility: off"
-dirty_map = url + "center=" + center + "&zoom=" + str(zoom) + "&size=" + size + "&maptype=" + maptype2 +"&key=" + api_key
-print(dirty_map)
-rDirty = requests.get(dirty_map)
-with open('dirtyMap.jpg', 'wb') as file:
-    file.write(rDirty.content)
+    # storage_client = storage.Client()
+    # bucket = storage_client.bucket(bucket_name)
+    # blob = bucket.blob(destination_blob_name)
 
-rDirty.close()
+    # blob.upload_from_filename(source_file_name)
 
-# using matpltolib to display the image
-plt.figure(figsize=(5, 5))
-img=mpimg.imread('map.jpg') #render the terrain
-dirtyImg=mpimg.imread('dirtyMap.jpg') #render the styled map
-imgplot = plt.imshow(img)
-dirtyImgPlot = plt.imshow(dirtyImg)
-plt.axis('off')
-plt.show()
-
-# # wb mode is stand for write binary mode
-# f = open('address of the file location ', 'wb')
-  
-# # r.content gives content,
-# # in this case gives image
-# f.write(r.content)
-  
-# # close method of file object
-# # save and close the file
-# f.close()
-
-
-# TODO: After we generate image, put into GCP bucket by making GCP API call
+    # print(
+    #     f"File {source_file_name} uploaded to {destination_blob_name}."
+    # )
 
 def get_coords(location, n, radius):
     url = 'https://nominatim.openstreetmap.org/search/' + urllib.parse.quote(location) +'?format=json'
@@ -100,16 +83,30 @@ def get_coords(location, n, radius):
         zoom = random.randint(17, 20)
         coordinates.append((lat, lon, zoom))
 
-    #print(coordinates)
+    # print(coordinates)
     return coordinates
-
-
-def upload_to_cloud(c):
-    pass
 
 locations = ["New York", "Boston"]
 coords = []
 for l in locations:
     coords.extend(get_coords(l, n=10, radius=5))
+
+temp_limit = 1
+count = 1
 for c in coords:
-    upload_to_cloud(c)
+    center = str(c[0]) + "," + str(c[1])
+    zoom = c[2]
+
+    """ Clean Map """
+    clean_image = get_map(center, zoom, "clean_" + center + ".jpg", "clean")  # what's a better name for the image files?
+    canny_clean_image = apply_canny(clean_image)
+    upload_blob(canny_clean_image)
+
+    """ Dirty Map """
+    dirty_image = get_map(center, zoom, "dirty_" + center + ".jpg", "dirty")
+    canny_dirty_image = apply_canny(clean_image)
+    upload_blob(canny_dirty_image)
+
+    count = count + 1
+    if (count > temp_limit):
+        break
