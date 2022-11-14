5d4cab4f3a77316c74b75bc1867eb14b4c81f411
Garamon <ollietoh@gmail.com>
2022-11-13 19:44:54 -0500

    added canny into pipeline

--- canny.py
+++ canny.py
@@ -2,11 +2,13 @@ import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
 
-img = cv.imread('images/clean.png',0)
-#img = cv.imread('images/osm.png',0)
-edges = cv.Canny(img, 20, 40)
-plt.subplot(121),plt.imshow(img,cmap = 'gray')
+#img = cv.imread('images/clean.png',0)
+img = cv.imread('images/neu.png',0)
+edges = cv.Canny(img, 150, 300)
+edges, thr = cv.threshold(edges, 200, 255, cv.THRESH_BINARY)
+
+plt.subplot(121),plt.imshow(img)
 plt.title('Original Image'), plt.xticks([]), plt.yticks([])
-plt.subplot(122), plt.imshow(edges,cmap = 'gray')
+plt.subplot(122), plt.imshow(edges)
 plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
 plt.show()
