40c30f725a88b2c7d645aa16f45eee020ccdfbf1
Garamon <ollietoh@gmail.com>
2022-11-18 17:07:17 -0500

    conv Autoencoder architecture implemented

--- autoencoder.py
+++ autoencoder.py
@@ -2,6 +2,8 @@ import torch
 from torchvision import datasets
 from torchvision import transforms
 import matplotlib.pyplot as plt
+from PIL import Image
+import numpy as np
 
 #Transforms images to a PyTorch Tensor
 tensor_transform = transforms.ToTensor()
@@ -62,7 +64,77 @@ class AE(torch.nn.Module):
 		encoded = self.encoder(x)
 		decoded = self.decoder(encoded)
 		return decoded
-                           
+
+
+class ConvAE(torch.nn.Module):
+    def __init__(self, encoded_space_dim):
+        super().__init__()
+        self.encoder_cnn = torch.nn.Sequential(
+            #640x640x3
+            torch.nn.Conv2d(1, 8, 64, stride=2, padding=1),
+            torch.nn.ReLU(True),
+            #290x290x8
+            torch.nn.Conv2d(8, 16, 64, stride=2, padding=1),
+            torch.nn.BatchNorm2d(16),
+            torch.nn.ReLU(True),
+            #115x115x16
+            torch.nn.Conv2d(16, 32, 64, stride=2, padding=1),
+            torch.nn.ReLU(True),
+            #27x27x32
+            torch.nn.Conv2d(32, 64, 16, stride=2, padding=1),
+            torch.nn.ReLU(True)
+            #7x7x64
+        )
+
+        self.flatten = torch.nn.Flatten(start_dim=1)
+        
+        self.encoder_lin = torch.nn.Sequential(
+            torch.nn.Linear(3136, 2048),
+            torch.nn.ReLU(True),
+            torch.nn.Linear(2048, 1024),
+            torch.nn.ReLU(True),
+            torch.nn.Linear(1024, encoded_space_dim)
+        )
+
+        self.decoder_lin = torch.nn.Sequential(
+            torch.nn.Linear(encoded_space_dim, 1024),
+            torch.nn.ReLU(True),
+            torch.nn.Linear(1024, 2048),
+            torch.nn.ReLU(True),
+            torch.nn.Linear(2048, 3136),
+            torch.nn.ReLU(True)
+        )
+
+        self.unflatten = torch.nn.Unflatten(dim=1, 
+        unflattened_size=(64, 7, 7))
+
+        self.decoder_conv = torch.nn.Sequential(
+            torch.nn.ConvTranspose2d(64, 32, 16, 
+            stride=2, output_padding=1),
+            torch.nn.BatchNorm2d(32),
+            torch.nn.ReLU(True),
+            torch.nn.ConvTranspose2d(32, 16, 64, stride=2, 
+            padding=1, output_padding=1),
+            torch.nn.BatchNorm2d(16),
+            torch.nn.ReLU(True),
+            torch.nn.ConvTranspose2d(16, 8, 64, stride=2, 
+            padding=1, output_padding=1),
+            torch.nn.BatchNorm2d(8),
+            torch.nn.ReLU(True),
+            torch.nn.ConvTranspose2d(8, 1, 64, stride=2, 
+            padding=1, output_padding=1)
+        )
+    
+    def forward(self, x):
+        x = self.encoder_cnn(x)
+        x = self.flatten(x)
+        x = self.encoder_lin(x)
+        x = self.decoder_lin(x)
+        x = self.unflatten(x)
+        x = self.decoder_conv(x)
+        x = torch.sigmoid(x)
+        return x
+
 # Model Initialization
 model = AE()
  
@@ -74,7 +146,7 @@ optimizer = torch.optim.Adam(model.parameters(),
                              lr = 1e-1,
                              weight_decay = 1e-8)
 
-epochs = 20 # takes 15 to 20 mins to execute, Initialize epoch = 1, for quick results
+epochs = 0 # takes 15 to 20 mins to execute, Initialize epoch = 1, for quick results
 outputs = []
 losses = []
 for epoch in range(epochs):
@@ -105,9 +177,16 @@ plt.xlabel('Iterations')
 plt.ylabel('Loss')
 
 # Plotting the last 100 values
-plt.plot(losses[-100:])
+#plt.plot(losses.detach().numpy()[-100:])
 
-for i, item in enumerate(image):
+conv_model = ConvAE(512)
+img = torch.Tensor(np.array(Image.open("images/canny/dirty/19.jpg")))
+img = torch.unsqueeze(torch.unsqueeze(img, 0), 0)
+out = conv_model(img).detach().numpy()
+out = np.squeeze(out, (0,1))
+plt.imshow(out)
+plt.show()
+"""for i, item in enumerate(image):
   # Reshape the array for plotting
   item = item.reshape(-1, 28, 28)
   plt.imshow(item[0])
@@ -117,4 +196,5 @@ for i, item in enumerate(reconstructed):
   item = item.reshape(-1, 28, 28)
   with torch.no_grad():
     plt.imshow(item[0])
-    plt.show()
+    plt.show()"""
+
