222cc8932fee4981e88b52eb83d698b25c83ed28
Jessica Yu <jessicayu62@gmail.com>
2022-11-21 19:29:29 -0500

    add csv creater

--- csvCreater.py
+++ csvCreater.py
@@ -10,24 +10,25 @@ storage_client = storage.Client()
 
     # Note: Client.list_blobs requires at least package version 1.17.0.
 blobs = storage_client.list_blobs('train_test_dataset')
+# blob_names = blobs.name
+list = [element.name for element in blobs]
 
 data = {
-    {
-        'dirty': 
-            blobs, 
-        'clean': 
-            blobs
-    }
+    'dirty': 
+        list, 
+    'clean': 
+        list
+    
 }
 df = pd.DataFrame(data)
-df['dirty'] = df['dirty'].str.contains('.')
-df['dirty'] = df['dirty'].str.contains('dirty')
-df['clean'] = df['clean'].str.contains('.')
-df['clean'] = df['clean'].str.contains('clean')
+df['dirty'] = df[df['dirty'].str.contains('.') == True]
+df['dirty'] = df[df['dirty'].str.contains('dirty') == True]
+df['clean'] = df[df['clean'].str.contains('.') == True]
+df['clean'] = df[df['clean'].str.contains('clean') == True]
 
 
 
-df = pd.DataFrame(data)
+# df = pd.DataFrame(data)
 csv_data = df.to_csv('dataset.csv')
-upload_blob(csv_data, 'dataset.csv')
+upload_blob('dataset.csv', 'dataset.csv')
 
