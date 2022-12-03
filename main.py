638125cb2ae85c2ae33c65780c2f34eb16f4daf8
Garamon <ollietoh@gmail.com>
2022-12-02 20:21:16 -0500

    implemented function to call the model endpoint

--- main.py
+++ main.py
@@ -1,36 +1,16 @@
-# creates n uniformly random samples within radius r from the location
-def get_coords(location, n, radius):
-    url = 'https://nominatim.openstreetmap.org/search/' + urllib.parse.quote(location) +'?format=json'
-    response = requests.get(url).json()
-    anchor_lat = float(response[0]["lat"]) #y
-    anchor_lon = float(response[0]["lon"]) #x
+from google.cloud import aiplatform
+import numpy as np
+from PIL import Image
 
-    #convert miles to lat degrees
-    r = radius / 69
+PROJECT_NUMBER = "careful-striker-367620"
+ENDPOINT_ID = None
 
-    coordinates = []
+#sends the image to the trained model endpoint and returns the output Image
+def denoise(input):
+    endpoint = aiplatform.Endpoint(
+        endpoint_name="projects/{PROJECT_NUMBER}/locations/us-central1/endpoints/{ENDPOINT_ID}")
+    x_test = np.asarray(input).astype(np.float32).tolist()
+    output = endpoint.predict(instances=x_test).predictions
+    output_image = Image.fromarray(output)
 
-    for _ in range(n):
-        #latitude can be +- radius from the anchor lat
-        lat = anchor_lat + random.uniform(-r, r)
-
-        # from circle eqn
-        delta_lon = math.sqrt(r**2 - (lat - anchor_lat)**2)
-        lon = anchor_lon + random.uniform(-delta_lon, delta_lon)
-
-        zoom = random.randint(17, 20)
-        coordinates.append((lat, lon, zoom))
-
-    #print(coordinates)
-    return coordinates
-
-
-def upload_to_cloud(c):
-    pass
-
-locations = ["New York", "Boston"]
-coords = []
-for l in locations:
-    coords.extend(get_coords(l, n=10, radius=5))
-for c in coords:
-    upload_to_cloud(c)
+    return output_image
